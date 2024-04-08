f93471d93c97bea7b8e2d742bacb209cf278ab5a
Brujini123 <147409948+Brujini123@users.noreply.github.com>
2024-04-07 22:46:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -38,3 +38,38 @@ function cargarProductos() {
 }
 
 cargarProductos();
+
+// Obtener el parámetro de consulta de la URL
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const productName = urlParams.get('id');
+
+// Función para cargar los detalles del producto
+function cargarDetallesProducto(productName) {
+    // Hacer una solicitud para obtener los detalles del producto según el nombre
+    fetch('productos.json')
+        .then(response => response.json())
+        .then(data => {
+            // Encontrar el producto por su nombre
+            const product = data.find(item => item.nombre === productName);
+
+            if (product) {
+                // Mostrar los detalles del producto
+                const productDetails = document.getElementById("product-details");
+                productDetails.innerHTML = `
+                    <h2>${product.nombre}</h2>
+                    <img src="${product.imagen}" alt="${product.nombre}">
+                    <p><strong>Precio:</strong> ${product.precio}</p>
+                    <p><strong>Descripción:</strong> ${product.descripcion}</p>
+                    <a href="javascript:history.back()" class="btn">Volver atrás</a>
+                `;
+            } else {
+                // Manejar el caso en el que el producto no se encuentra
+                console.error('Producto no encontrado');
+            }
+        })
+        .catch(error => console.error('Error al cargar los detalles del producto:', error));
+}
+
+// Llamar a la función para cargar los detalles del producto
+cargarDetallesProducto(productName);
