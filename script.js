a24c4b805681a4ced0911a9e3573e008932381fd
Erik <erik.alj_ram@hotmail.com>
2024-04-30 07:39:18 -0600

    hola

--- script.js
+++ script.js
@@ -86,22 +86,9 @@ cargarDetallesProducto(productName);
                     <p>${product.descripcion}</p>
                     <div class="product-footer">
                         <div class="product-price">${product.precio}</div>
-                        
-                        <button class="product-btn" data-producto="${product.nombre}"><i class="bi bi-arrow-left-right">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-right" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5m14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5"/>
-                        </svg>
-                        </i>
-                        Compare
-                        </button>
 
-                        <button class="product-btn" data-producto="${product.nombre}">
-                        <i class="bi bi-suit-heart-fill">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-suit-heart-fill" viewBox="0 0 16 16">
-                        <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1"/>
-                        </svg>
-                        </i>
-                        Remember</button>
+                        <button type="button" class="btn btn-primary" data-producto="${product.nombre}">Ver mas</button>
+
 
                     </div>
                 </div>
